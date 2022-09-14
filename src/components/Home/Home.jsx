2bade63d598f870d068ce1b920cb0a57a80f2c04
Yoshimivi <yoshimisuzaki@hotmail.com>
2022-09-14 11:37:00 -0300

    regex input
    uptade on regex input to fix bug with zeros after comma

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -28,9 +28,9 @@ useEffect(() => {
   const handleBillNoTipAndCurrencyMask = (e) => {
     setBillValue(prevBillValue => ({...prevBillValue, billTotalNoTip: Number(e.target.value)}))
     let value = e.target.value
-    value = value.replace(/\D/g, "")
-    value = value.replace(/(\d)(\d{2})$/, "$1.$2")
+    value = value.replace(/\D/g, "")   
     value = value.replace(/\b0/, "")
+    value = value.replace(/(\d)(\d{2})$/, "$1.$2")
     // value = value.replace(/(?=(\d{3})+(\D))\B/g, ",")
     e.target.value = value
     return e
